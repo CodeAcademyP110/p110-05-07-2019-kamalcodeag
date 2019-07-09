a156879973c774b4e45abdbda8d697eae1122dc6
Kamal Guliyev <kamalag@code.edu.az>
2019-07-09 19:55:52 +0400

    *** UPDATED 2  - the link: /properties/details/id ***

--- Home/Practice2/Practice2/Practice2/Controllers/PropertiesController.cs
+++ Home/Practice2/Practice2/Practice2/Controllers/PropertiesController.cs
@@ -16,9 +16,20 @@ namespace Practice2.Controllers
         {
             efdb = efdb2;
         }
-        public IActionResult Details(int id)
+        public IActionResult Details(int? id)
         {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
             House house = efdb.Houses.Include(h => h.HousePhotos).FirstOrDefault(h => h.Id == id);
+
+            if(house == null)
+            {
+                return NotFound();
+            }
+
             return View(house);
         }
     }
